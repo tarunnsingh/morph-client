1db200d3ed7fa28af3b9c596226b2d4587c5935d
Priyansh Singh <thetseffect@gmail.com>
2020-09-29 21:06:40 +0530

    Update README

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 <img src="https://cdn.dribbble.com/users/470545/screenshots/2153975/face-morphing.gif" width="300"/>
 
 ## Face Morphing (Client Side Script) - See Client Side [Here](https://github.com/tarunnsingh/morph-server).
-## Project Demo 
+
+## Project Demo :nerd_face:
+
 [![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/_ThVHciEj4g/0.jpg)](http://www.youtube.com/watch?v=_ThVHciEj4g)
 
-### What is Morphing?
+### What is Morphing? :thinking:
+
 The face morphing algorithm morphs between faces using a common set of feature points, placed by hand on each face. To morph between two faces, you need to warp both faces to a common shape so they can be blended together.
 
-### Features:
-1. **FilePond**: FilePond provides a smooth UX for uploading and kind of files. A wrapper built for ReactJS makes our job easy. Check [this]()
+### About this Client Side Repo :monocle_face:
+
+This will run only when you are up and running the server.
+
+### Uses:
+
+1. **FilePond**: FilePond provides a smooth UX for uploading and kind of files. A wrapper built for ReactJS makes our job easy.
 2. **Material UI**: Provides easy styling functionalities to React components.
 
-### Run Locally:
-Check Steps in the [SERVER](https://github.com/tarunnsingh/morph-server) repo.
+### Run Locally: :rocket:
 
-**Thank You**
+1. [Important] Before running this client side script, make sure you have started your server succesfully. Check Steps in the [SERVER](https://github.com/tarunnsingh/morph-server) repo.
+2. Now clone this repo to any other location than that of the server.
+3. If in the Server Side steps you changes the PORT number then update it in package.json -> PROXY. Otherwise leave it as is.
+4. Install dependecies with `npm install`.
+5. Run the script with `npm start`.
+6. Go to ```http://localhost:3000" in browse to test the project.
+
+### Contribute
+
+Check and put up Issues and let me know the features which you wish to add before making a PR.
+
+### Leave a :star: if you found this helpful.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, {Component, Fragment } from 'react';
-import { UploadBox, Footer }  from './components/index'
+import React, { Component, Fragment } from "react";
+import { UploadBox, Footer } from "./components/index";
 import { Typography, Toolbar, AppBar, Button, Grid } from "@material-ui/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner, faCircleNotch, faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
-import styles from './App.module.css'
-import axios from 'axios'
+import {
+  faSpinner,
+  faCircleNotch,
+  faArrowAltCircleDown,
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "./App.module.css";
+import axios from "axios";
 // import FileDownload from 'js-file-download'
 
 export default class App extends Component {
@@ -12,64 +16,62 @@ export default class App extends Component {
     title: "Face Morping",
     filenames: [],
     disabled: true,
-    connectedName: '',
+    connectedName: "",
     loading: false,
-    sessionComplete: false
+    sessionComplete: false,
   };
 
   createConnectedName = () => {
     if (this.state.filenames.length === 2) {
-      const connectedName = this.state.filenames[0] + '|' + this.state.filenames[1]
-      this.setState({ connectedName })
+      const connectedName =
+        this.state.filenames[0] + "|" + this.state.filenames[1];
+      this.setState({ connectedName });
     }
-  }
+  };
 
   handleFileName = (filename) => {
     this.setState({
-      filenames: [...this.state.filenames, filename]
-    })
-    if(this.state.filenames.length === 2){
+      filenames: [...this.state.filenames, filename],
+    });
+    if (this.state.filenames.length === 2) {
       this.setState({
-        disabled: false
-      })
-    this.createConnectedName()
+        disabled: false,
+      });
+      this.createConnectedName();
     }
-  }
+  };
 
   handleClick = () => {
-    if(!this.state.disabled){
-      this.setState({loading: true})
+    if (!this.state.disabled) {
+      this.setState({ loading: true });
       axios({
         url: `/api/morph/${this.state.connectedName}`,
-        method: 'GET',
-        responseType: 'blob',
+        method: "GET",
+        responseType: "blob",
       }).then((response) => {
-         const url = window.URL.createObjectURL(new Blob([response.data]));
-         const link = document.createElement('a');
-         link.href = url;
-         link.setAttribute('download', 'Morph.gif'); //or any other extension
-         document.body.appendChild(link);
-         link.click();
-         this.resetPage()
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "Morph.gif"); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+        this.resetPage();
       });
     }
-  }
-  componentDidUpdate = () => {
-
-  }
+  };
+  componentDidUpdate = () => {};
 
   resetPage = () => {
     this.setState({
       filenames: [],
-    disabled: true,
-    connectedName: '',
-    loading: false
-    })
-  }
-
+      disabled: true,
+      connectedName: "",
+      loading: false,
+    });
+  };
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <Fragment>
         <AppBar position="static">
@@ -77,26 +79,42 @@ export default class App extends Component {
             <Typography variant="h6">{this.state.title}</Typography>
           </Toolbar>
         </AppBar>
-     <Grid container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      className={styles.button}>
-      <Grid item xs={12} md = {6} lg={6}>
-      <Button variant="contained" color="primary" disabled={this.state.disabled} onClick={this.handleClick}>
-      <Typography variant="button"><FontAwesomeIcon icon={faArrowAltCircleDown} /> Download Morph GIF </Typography>
-      </Button>
-      </Grid>
-      {this.state.loading && 
-      <Fragment>
-      <Typography className={styles.typo} color="primary" variant="caption">Building Morph, your download will begin in a few seconds...</Typography>
-      <FontAwesomeIcon icon={faCircleNotch} spin />
-      </Fragment>
-      }
-      <UploadBox handleFileName = {this.handleFileName}/>
-      </Grid>
-      <Footer />
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          className={styles.button}
+        >
+          <Grid item xs={12} md={6} lg={6}>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={this.state.disabled}
+              onClick={this.handleClick}
+            >
+              <Typography variant="button">
+                <FontAwesomeIcon icon={faArrowAltCircleDown} /> Download Morph
+                GIF{" "}
+              </Typography>
+            </Button>
+          </Grid>
+          {this.state.loading && (
+            <Fragment>
+              <Typography
+                className={styles.typo}
+                color="primary"
+                variant="caption"
+              >
+                Building Morph, your download will begin in a few seconds...
+              </Typography>
+              <FontAwesomeIcon icon={faCircleNotch} spin />
+            </Fragment>
+          )}
+          <UploadBox handleFileName={this.handleFileName} />
+        </Grid>
+        <Footer />
       </Fragment>
     );
   }
